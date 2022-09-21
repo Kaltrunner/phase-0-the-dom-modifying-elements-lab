17a1a9a5593f830ddd26497f846a8d32272d3d1f
Erik Russell <erikrussells@gmail.com>
2022-09-21 13:38:30 -0400

    commit tree c9174ed14f768cb9e52868273be37759a503e9c5

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here!
+// const main = document.querySelector("#main")
+// main.remove();
+
+// const newHeader = document.createElement("h1");
+// newHeader.setAttribute("id","victory");
+
+// newHeader.innerHTML = "Kaltrunner is the champion";
+// document.body.append(newHeader);
+
+
+
+// const main = document.querySelector("#main");
+// main.remove();
+
+// const newHeader = document.createElement("h1");
+// newHeader.setAttribute("id", "victory");
+
+// newHeader.innerHTML = "Erik is the champion";
+// document.body.append(newHeader);
+
+
+
+//const main = document.querySelector("#main");
+main.remove();
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+
+newHeader.innerHTML = "Kaltrunner is the champion";
+document.body.append(newHeader);
